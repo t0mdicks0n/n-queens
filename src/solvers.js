b8e5ec01b6fcd38148d55f74c3748063746d6df2
Hack Reactor Student <student@hackreactor.com>
2017-04-06 14:27:13 -0700

    MVP of findQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -30,14 +30,12 @@ window.findNRooksSolution = function(n) {
       }
     }
   }
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  // var solution = board.rows(); //fixme
 
   // function recurseOverBoard(board, depth) {
   //   if (depth === n) {
@@ -64,18 +62,6 @@ window.countNRooksSolutions = function(n) {
   //       }
   //         // test for conflict
   //     }
-  //     for (var j = 0; j < board.rows()[i].length; j++) {
-  //       board.rows()[i][j].set(1);
-
-  //       // get the specific row 
-  //       // for the row move the rook one step
-  //       // Set the row to the new modified row
-
-  //       board.set(i, [])
-
-  //       // board.set(0, [1, 0, 0, 0]);
-
-  //     }
   //   }
     
 
@@ -84,13 +70,52 @@ window.countNRooksSolutions = function(n) {
 
   // recurseOverBoard(board);
 
-  console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
-  return solutionCount;
+  // console.log('Number of solutions for ' + n + ' rooks:', solution);
+  // return solution;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  var board = new Board({'n':n});
+  var solution;
+
+  function recurseOverBoard(board, depth) {
+    if (depth === n) {
+      // return the board
+      if (solution === undefined) {
+        solution = JSON.parse(JSON.stringify(board.rows()));
+      }
+
+      return;
+    }
+    // iterate over board
+    // If var i is indefined, i === 0
+    for (var i = 0; i < board.rows().length; i++) {
+      // get our row
+      var row = board.get(i)
+      // iterate over the row
+      for (var k = 0; k < row.length; k++) {
+        // for each iteration we modify the board
+        if (row[k] !== 1) {
+          board.togglePiece(i, k)
+          // check for conflict on the table
+          if (!board.hasAnyQueensConflicts()) {
+            // if no conflict recurse on this table
+            // recurse on the board
+            recurseOverBoard(board, depth + 1, i, k);
+          }
+          board.togglePiece(i, k);
+        }
+          // test for conflict
+      }
+    }
+    
+
+      // for each for each square recurse on new board and depth + 1
+  }
+
+  recurseOverBoard(board, 0);
+
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
