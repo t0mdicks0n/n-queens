6f5c49124871e3947d088b03b4f91d60600e55c3
Hack Reactor Student <student@hackreactor.com>
2017-04-06 12:07:07 -0700

    Implemented findNRooksSolution. Onto countNRooksSolution.

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,20 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+
+  var board = new Board({'n':n});
+  var solution = board.rows(); //fixme
+
+  for (var i = 0; i < board.rows().length; i++) {
+    for (var j = 0; j < board.rows()[i].length; j++) {
+      if (board.rows()[i][j] === 0) {
+        board.togglePiece(i, j);
+        if (board.hasRowConflictAt(i) || board.hasColConflictAt(j)) {
+          board.togglePiece(i, j);
+        }
+      }
+    }
+  }
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
@@ -25,6 +38,51 @@ window.findNRooksSolution = function(n) {
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
   var solutionCount = undefined; //fixme
+
+  // function recurseOverBoard(board, depth) {
+  //   if (depth === n) {
+  //     // return the board
+  //     solution = board;
+  //     return board;
+  //   }
+  //   // iterate over board
+  //   for (var i = 0; i < board.rows().length; i++) {
+
+  //     // get our row
+  //     var row = board.get(i)
+  //     // iterate over the row
+  //     for (var k = 0; k < row.length; k++) {
+  //       // for each iteration we modify the board
+  //       if (row[k] !== 1) {
+  //         board.togglePiece(i, k)
+  //         // check for conflict on the table
+  //         if (!hasRowConflictAt(i) && !hasColConflictAt(k)) {
+  //           // if no conflict recurse on this table
+  //           // recurse on the board
+  //           recurseOverBoard(board, )
+  //         }
+  //       }
+  //         // test for conflict
+  //     }
+  //     for (var j = 0; j < board.rows()[i].length; j++) {
+  //       board.rows()[i][j].set(1);
+
+  //       // get the specific row 
+  //       // for the row move the rook one step
+  //       // Set the row to the new modified row
+
+  //       board.set(i, [])
+
+  //       // board.set(0, [1, 0, 0, 0]);
+
+  //     }
+  //   }
+    
+
+  //     // for each for each square recurse on new board and depth + 1
+  // }
+
+  // recurseOverBoard(board);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
